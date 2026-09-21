696ee0d1cd102fead9f7188f3d5f97f0f2f80e8f
ahmadamjadd <muhammadahmadamjad0@gmail.com>
2026-09-21 14:43:20 +0500

    infra: import ECR and restore ssm

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,13 @@ resource "aws_ssm_parameter" "learning_example" {
   
   description = "A safe test parameter to learn Terraform"
 }
+
+# 3. Import our existing ECR Repository
+resource "aws_ecr_repository" "backend" {
+  name                 = "filestorage-backend"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
