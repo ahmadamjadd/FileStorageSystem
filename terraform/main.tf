8d0e04d8db9f04ca52d8b7eaaec28cebf87c1bea
ahmadamjadd <muhammadahmadamjad0@gmail.com>
2026-09-21 16:05:54 +0500

    infra: automate terraform deployments via github actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ resource "aws_ssm_parameter" "learning_example" {
   name  = "/filestorage/learning/hello-world"
   type  = "String"
   value = "Hello from Terraform!"
-  
+
   description = "A safe test parameter to learn Terraform"
 }
 
