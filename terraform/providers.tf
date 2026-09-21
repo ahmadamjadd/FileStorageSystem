032ce781d85cb1b54035d28fd07995c85a142633
ahmadamjadd <muhammadahmadamjad0@gmail.com>
2026-09-21 15:41:41 +0500

    infra: update terraform to use native s3 locking

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,8 +5,18 @@ terraform {
       version = "~> 6.0"
     }
   }
+  backend "s3" {
+    bucket       = "filestorage-tf-state-12345"
+    key          = "global/s3/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+
 }
 
 provider "aws" {
   region = var.aws_region
 }
+
+
